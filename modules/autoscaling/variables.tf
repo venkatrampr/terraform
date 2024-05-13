30226d27d875d90e169cbc1158ca5e561e4f7736
Venkat Polamreddy <143093891+venkatrampr@users.noreply.github.com>
2024-05-13 17:19:51 -0500

    Create variables.tf

---
+++ modules/autoscaling/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc" {}
+variable "ami" {}
+variable "instance_type" {}
+# variable "pub_subnets" {}
+# variable "avail_zones" {}
+variable "pub_subnets" {}
+variable "lb_dns" {}
+# variable "subnet_ids" {}
+variable "pub_target_group" {}
+variable "pri_subnets" {}
+variable "pri_target_group" {}
